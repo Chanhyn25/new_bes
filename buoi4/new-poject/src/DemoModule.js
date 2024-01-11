acbc3e782e3220833c31fb01aa312a1d24bdf4dc
NguyenThiHanh <Hanhntph34748@fpt.edu.vn>
2024-01-11 21:48:48 +0700

    commit

---
+++ buoi4/new-poject/src/DemoModule.js
@@ -0,0 +1,5 @@
+export const a= 10;
+export const b = 20;
+export function sum(a,b){
+    return a + b;
+}
---
+++ buoi4/new-poject/src/main.jsx
@@ -0,0 +1,36 @@
+
+import ReactDOM from 'react-dom/client';
+import {a,b} from"./DemoModule";
+const myName = "Nguyen Yen Nhi";
+const myAge = 20;
+const isMarried = true;
+const myInfo = {
+  name: "Hanh",
+  age:30,
+
+};
+const showName = (name) => {
+  return `<h1>Ten toi la ${name}</h1>`;
+};
+//component
+const ShowName = () => {
+  return <h1> Ten toi la Hanh </h1>;
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+    <h1>Hello react {myName}</h1>
+    <p>Tuoi : {myAge}</p>
+    <p>Tình trạng: {isMarried ? "hihi" : "haha"}</p>
+    <p>hong tin: {myInfo.name}</p>
+    <p>Function : {showName("Nhi")}</p>
+    <hr />
+    <h2>Component</h2>
+    <ShowName></ShowName>
+    <div>
+  <h1 className="title">Hello</h1>
+  <h2 classname="abc">Hello wword</h2>
+</div>
+{sum(a,b)}
+  </div>
+);
